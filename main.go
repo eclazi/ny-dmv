6b398c803a8e864e776f293942822122b5d9561f
Joss <joss@jossgray.net>
2025-04-02 23:19:58 -0400

    Reimplement CLI begin

--- main.go
+++ main.go
@@ -1,134 +1,41 @@
 package main
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"time"
+import (
+	"context"
+	"fmt"
+	"os"
 
-// 	"github.com/urfave/cli/v3"
-// )
+	"github.com/urfave/cli/v3"
+)
 
-// func printServices() {
-// 	siteData, err := getSiteData()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func printServices() {
 
-// 	for _, serviceType := range siteData.ServiceTypes {
-// 		fmt.Println(serviceType.CategoryDescription)
-// 		for _, service := range serviceType.ServiceTypes {
-// 			fmt.Println("    ", service.TypeId, service.Name)
-// 		}
-// 	}
-// }
+	client := NewClient() 
+	services, err := client.GetServices()
+	if err != nil {
+		panic(err)
+	}
 
-// func collectLocations(serviceType int) map[LocationId]string {
-// 	locs, err := getLocationsByCounty(serviceType)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	for _, service := range services {
+		fmt.Println(service.Name)
+	}
+}
 
-// 	result := make(map[LocationId]string)
-// 	for _, county := range *locs {
-// 		for _, loc := range county.Locations {
-// 			result[loc.Id] = loc.Name
-// 		}
-// 	}
+func main() {
 
-// 	return result
-// }
+	cmd := &cli.Command{
+		Commands: []*cli.Command{
+			{
+				Name: "services",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					printServices()
+					return nil
+				},
+			},
+		},
+	}
 
-// func printLocations(serviceType int) {
-// 	loc, err := getLocationsByCounty(serviceType)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	for _, county := range *loc {
-// 		fmt.Println(county.County)
-// 		for _, location := range county.Locations {
-// 			fmt.Println("    ", location.Id, location.Name, location.City)
-// 		}
-// 	}
-// }
-
-// func earliestAppointment(service int, locations []LocationId) (*time.Time, LocationId) {
-// 	var earliestTime time.Time
-// 	var earliestLoc LocationId
-
-// 	for _, loc := range locations {
-// 		apt, err := getNextAvailableAppointment(loc, service)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-
-// 		if earliestTime.IsZero() || apt.Before(earliestTime) {
-// 			earliestTime = *apt
-// 			earliestLoc = loc
-// 		}
-// 	}
-
-// 	return &earliestTime, earliestLoc
-// }
-
-// func getLocationName(locationId LocationId, serviceId int) string {
-// 	nameMap := collectLocations(serviceId)
-// 	return nameMap[locationId]
-// }
-
-// func main() {
-
-// 	cmd := &cli.Command{
-// 		Commands: []*cli.Command{
-// 			{
-// 				Name: "services",
-// 				Action: func(ctx context.Context, cmd *cli.Command) error {
-// 					printServices()
-// 					return nil
-// 				},
-// 			},
-// 			{
-// 				Name: "locations",
-// 				Action: func(ctx context.Context, cmd *cli.Command) error {
-// 					v, err := strconv.Atoi(cmd.Args().First())
-// 					if err != nil {
-// 						panic(err)
-// 					}
-
-// 					printLocations(v)
-// 					return nil
-// 				},
-// 			},
-// 			{
-// 				Name: "appointmenat",
-// 				Action: func(ctx context.Context, cmd *cli.Command) error {
-
-// 					service, err := strconv.Atoi(cmd.Args().First())
-// 					if err != nil {
-// 						panic(err)
-// 					}
-
-// 					var locations []LocationId
-// 					for _, arg := range cmd.Args().Slice()[1:] {
-// 						if v, err := strconv.Atoi(arg); err == nil {
-// 							locations = append(locations, LocationId(v))
-// 						}
-// 					}
-
-// 					earliest, earliestLoc := earliestAppointment(service, locations)
-
-// 					locName := getLocationName(earliestLoc, service)
-// 					fmt.Println(locName, " - ", earliest.Format(time.RFC1123))
-
-// 					return nil
-// 				},
-// 			},
-// 		},
-// 	}
-
-// 	if err := cmd.Run(context.Background(), os.Args); err != nil {
-// 		panic(err)
-// 	}
-// }
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		panic(err)
+	}
+}
